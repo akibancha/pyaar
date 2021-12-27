0c2633f2cc11435229046d84fb3e537caa39ef19
akibancha <sesametea@riseup.net>
2021-12-27 02:12:59 +0100

    better draw ranges

--- src/interface/render_map.py
+++ src/interface/render_map.py
@@ -3,50 +3,62 @@ import random
 from typing import Tuple, Set
 
 
-def get_draw_range(pointer_pos: Tuple[int, int],
-                   map_size: Tuple[int, int],
-                   window_size: Tuple[int, int]) -> Tuple[range, range]:
+def get_draw_range(
+        pointer_pos: Tuple[int, int],
+        map_size: Tuple[int, int],
+        window_size: Tuple[int, int],
+        buffer: int
+    ) -> Tuple[range, range]:
 
+    # set pointer pos
     pointer_y, pointer_x = pointer_pos
 
-    win_size_y, win_size_x = window_size
-    win_size_y += -2
-    win_size_x += -2
+    # set window size
+    win_height, win_width = window_size
+    win_height += -buffer * 2
+    win_width += -buffer * 2
 
-    map_size_y, map_size_x = map_size
+    # set map size
+    map_height, map_width = map_size
 
-    draw_y_begin = pointer_y - win_size_y // 2
-    draw_y_end = pointer_y + win_size_y // 2
+    # correct draw sizes in case of a very small map
+    if win_height > map_height:
+        win_height = map_height
+    if win_width > map_width:
+        win_width = map_width
 
-    draw_x_begin = pointer_x - win_size_x // 2
-    draw_x_end = pointer_x + win_size_x // 2
+    # set the y range begin and end
+    draw_y_begin = pointer_y - win_height // 2
+    draw_y_end = pointer_y + win_height // 2
+    # correct for an odd numbered window size
+    if win_height % 2 != 0:
+        draw_y_end += 1
 
-    if pointer_y >= map_size_y - win_size_y // 2:
-        fix_value = pointer_y - map_size_y
-        draw_y_begin += -fix_value - win_size_y // 2
-        draw_y_end = map_size_y
-
-    if pointer_x >= map_size_x - win_size_x // 2:
-        fix_value = pointer_x - map_size_y
-        draw_x_begin += -fix_value - win_size_x // 2
-        draw_x_end = map_size_x
-
-    if draw_y_begin <= 0:
+    # correct draw ranges
+    if draw_y_begin < 0:
         draw_y_begin = 0
-        if win_size_y < map_size_y:
-            draw_y_end = win_size_y
-        else:
-            draw_y_end = map_size_y
+        draw_y_end = win_height
+    if draw_y_end > map_height:
+        draw_y_begin = map_height - win_height
+        draw_y_end = map_height
 
-    if draw_x_begin <= 0:
+    # do the same with the x range
+    # vvv
+    draw_x_begin = pointer_x - win_width // 2
+    draw_x_end = pointer_x + win_width // 2
+    # correct for an odd numbered window size
+    if win_width % 2 != 0:
+        draw_x_end += 1
+
+    # correct draw ranges
+    if draw_x_begin < 0:
         draw_x_begin = 0
-        if win_size_x < map_size_x:
-            draw_x_end = win_size_x
-        else:
-            draw_x_end = map_size_x
+        draw_x_end = win_width
+    if draw_x_end > map_width:
+        draw_x_begin = map_width - win_width
+        draw_x_end = map_width
 
-    return (range(draw_y_begin, draw_y_end),
-            range(draw_x_begin, draw_x_end))
+    return range(draw_y_begin, draw_y_end), range(draw_x_begin, draw_x_end)
 
 
 def render(game,
@@ -63,17 +75,19 @@ def render(game,
     game_tile_map = game_map["map_array"]
     game_map_height = game_map["map_height"]
     game_map_width = game_map["map_width"]
+    buffer = 3
     draw_y, draw_x = get_draw_range(pointer_pos=pointer_pos,
                                     map_size=(game_map_height, game_map_width),
-                                    window_size=(window_size_y, window_size_x))
+                                    window_size=(window_size_y, window_size_x),
+                                    buffer=buffer)
 
     debug_string = f"({draw_y}, {draw_x}, {game.round}, {game.pointer_pos})"
     map_window.addstr(0, 1, debug_string)
     fov_set: Set[Tuple[int, int]] = set()
     if fov:
         fov_set = game.pool.entities[fov_entity]["FOV"]
-    for window_y, map_y in enumerate(draw_y, 1):
-        for window_x, map_x in enumerate(draw_x, 1):
+    for window_y, map_y in enumerate(draw_y, buffer):
+        for window_x, map_x in enumerate(draw_x, buffer):
             ent_id = game_tile_map[map_y][map_x][-1]
             ent = game.pool.entities[ent_id]
             char = ent.get("char")
--- src/systems/move.py
+++ src/systems/move.py
@@ -1,3 +1,5 @@
+from typing import Any, Optional
+
 import ecs
 
 
@@ -5,31 +7,58 @@ class MoveSystem(ecs.System):
 
     def update(self):
 
+        if self.pool.etc["game"].config["debug"]["debug_log_mgs_system_report"] is True:
+            mgs0: str = f" | Update Cycle {self.pool.etc['game'].round} :: MoveSystem started"
+            self.pool.etc["game"].debug_log.add(mgs0)
+
+        e_id: int
         for e_id in self.act_on(["velo", "pos"]):
+
+            ent: dict[str, Any]
             ent = self.entities[e_id]
+
+            vy: int
+            vx: int
             vy, vx = ent["velo"]
+
+
+            y: int
+            x: int
             y, x, map_id = ent["pos"]
+
+            dy: int
+            dx: int
             dy, dx = vy + y, vx + x
+
+            name: Optional[str]
             name = ent.get("name")
+
             if not name:
                 name = f"<entity {e_id}>"
+
+            ent_in: int
             for ent_in in self.entities[map_id]["map_array"][dy][dx]:
-                if (self.entities[ent_in].get("unpassable") and not
-                    (self.pool.etc["game"].config["debug"]
-                                                 ["disable_collision"])):
-                    tile = self.entities[ent_in]
-                    tile_name = tile.get("name")
-                    log_role = "something"
+                if self.entities[ent_in].get("blocks_path") and not self.pool.etc["game"].config["debug"]["disable_collision"]:
+
+                    tile: dict[str, Any] = self.entities[ent_in]
+                    tile_name: Optional[str] = tile.get("name")
+
+                    log_role: str = "something"
+
                     if not tile_name:
                         tile_name = f"<entity {ent_in}>"
+
                     log = f"{name} bumps into {log_role} ({tile_name})"
+
                     if (y, x) in self.pool.etc["game"].player["FOV"]:
                         self.pool.etc["game"].log.append(log)
                     break
             else:
+
                 self.entities[e_id]["pos"] = (dy, dx, map_id)
                 self.pool.entities[map_id]["map_array"][dy][dx].append(e_id)
                 self.pool.entities[map_id]["map_array"][y][x].remove(e_id)
+
             self.pool.remove_component_from_entity("velo", e_id)
             if ent.get("FOV"):
                 self.pool.add_components_to_entity({"update_fov": True}, e_id)
