d449c7908d1c65eb3df44e9aba37eee3d2d61e6b
akibancha <sesametea@riseup.net>
2022-01-18 04:47:17 +0100

    log mgs are no longer reversed

--- src/interface/render_log.py
+++ src/interface/render_log.py
@@ -2,11 +2,12 @@
 def render(map_window,
            game):
     win_y, win_x = map_window.getmaxyx()
-    line = 1
+    line = 2
     for elem in game.log[::-1]:
-        map_window.addstr(line, 1, elem)
+        map_window.addstr(win_y - line, 1, elem)
         line += 1
-        if line == win_y - 1:
+        if win_y - line < 1:
             break
+        
     map_window.refresh()
 
